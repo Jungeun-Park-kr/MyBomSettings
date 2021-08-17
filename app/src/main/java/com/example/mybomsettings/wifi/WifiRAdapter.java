4adb709992941e316c078ff88d00e722afbf1772
JungEun Park <je991025@gmail.com>
2021-08-17 17:10:58 +0900

    feat : Add remove from saved wifi

--- app/src/main/java/com/example/mybomsettings/wifi/WifiRAdapter.java
+++ app/src/main/java/com/example/mybomsettings/wifi/WifiRAdapter.java
@@ -42,7 +42,6 @@ public class WifiRAdapter extends RecyclerView.Adapter<WifiRAdapter.ViewHolder>{
 
     Dialog connectedDialog; // 연결된 WiFi 다이얼로그
     Dialog ConnectDialog; // WiFi 다이얼로그
-    Dialog saveDialog; // 저장된 WiFi 다이얼로그
 
 
     private static final String TAG = "MyTag:WiFiRAdapter)";
@@ -89,9 +88,6 @@ public class WifiRAdapter extends RecyclerView.Adapter<WifiRAdapter.ViewHolder>{
                 //connectedDialog.setContentView();
                 showConnectedDialog(v, pos);
             } else if (state == WiFi.WIFI_SAVED) { // 저장됨
-                saveDialog = new Dialog(v.getContext());
-                saveDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                //saveDialog.setContentView();
                 showSaveDialog(v, pos);
             } else if (state == WiFi.WIFI_CONNECTING) { // 연결중
                 ;
@@ -190,52 +186,7 @@ public class WifiRAdapter extends RecyclerView.Adapter<WifiRAdapter.ViewHolder>{
     private void showConnectedDialog(View v, int pos) { // 연결된 WiFi 다이얼로그
         //connectedDialog.show();
         // 다시 한번 더 연결 처리 in here
-    }
 
-    public void connect(Context context, String ssid, String password) {
-
-        WifiConfiguration wifiConf = null;
-        WifiConfiguration savedConf = null;
-
-        //existing configured networks
-        WifiManager mWifiManager = (WifiManager)context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        List<WifiConfiguration> list = mWifiManager.getConfiguredNetworks();
-
-        if(list!=null) {
-            for( WifiConfiguration i : list ) {
-                if (i.SSID != null && i.SSID.equals("\"" + ssid + "\"")) {
-                    Log.d(TAG, "existing network found: " + i.networkId + " " + i.SSID);
-                    savedConf = i;
-                    break;
-                }
-            }
-        }
-
-        if(savedConf!=null) {
-            Log.d(TAG, "coping existing configuration");
-            wifiConf = savedConf;
-        } else {
-            Log.d(TAG, "creating new configuration");
-            wifiConf = new WifiConfiguration();
-        }
-
-        wifiConf.SSID = String.format("\"%s\"", ssid);
-        wifiConf.preSharedKey = String.format("\"%s\"", password);
-
-        int netId;
-
-        if(savedConf!=null) {
-            netId = mWifiManager.updateNetwork(wifiConf);
-            Log.d(TAG, "configuration updated " + netId);
-        } else {
-            netId = mWifiManager.addNetwork(wifiConf);
-            Log.d(TAG, "configuration created " + netId);
-        }
-
-        mWifiManager.saveConfiguration();
-        mWifiManager.disconnect();
-        mWifiManager.enableNetwork(netId, true);
-        mWifiManager.reconnect();
     }
 
     private void showConnectDialog(View v, int pos) { // WiFi 다이얼로그
@@ -321,43 +272,75 @@ public class WifiRAdapter extends RecyclerView.Adapter<WifiRAdapter.ViewHolder>{
      *
      */
     private void showSaveDialog(View v, int pos) { // 저장된 WiFi 다이얼로그 (클릭시 연결 시도)
-        // saveDialog.show();
         Context baseContext = v.getContext();
         String networkSSID = myWiFiList.get(pos).getSsid();
-        ScanResult scanResult = myWiFiList.get(pos).getScanResult();
-
-        //remember id
         WifiManager wifiManager = (WifiManager)v.getContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        int netId = -1;
-        for (WifiConfiguration tmp : wifiManager.getConfiguredNetworks()) {
-            if (tmp.SSID.equals("\"" +networkSSID+"\"")) {
-                // Log.i(TAG, "똑같은거 찾음:"+networkSSID);
 
-
-                netId = tmp.networkId;
-                wifiManager.enableNetwork(netId, true);
-                break;
-            }
-        }
-        if (netId == -1) { // 연결 실패한 경우
-            Log.e(TAG, "addNetwork() returns -1.");
-            myWiFiList.get(pos).setState(WiFi.WIFI_ERROR); // 오류 발생
-            notifyDataSetChanged();
-            AlertDialog.Builder failDialog = new AlertDialog.Builder(baseContext);
-            failDialog.setTitle("연결을 실패했습니다. 다시 시도해주세요");
-            failDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    ;
+        AlertDialog.Builder saveDialog = new AlertDialog.Builder(baseContext);
+        saveDialog.setTitle(networkSSID); // 다이얼로그 텍스트 : WiFi SSID명
+        saveDialog.setPositiveButton("연결", new DialogInterface.OnClickListener() { // 저장된 WiFi 연결 시작
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int netId = -1;
+                for (WifiConfiguration tmp : wifiManager.getConfiguredNetworks()) {
+                    if (tmp.SSID.equals("\"" +networkSSID+"\"")) {
+                        // Log.i(TAG, "똑같은거 찾음:"+networkSSID);
+                        netId = tmp.networkId;
+                        wifiManager.enableNetwork(netId, true);
+                        break;
+                    }
                 }
-            });
-            failDialog.show();
-        }
-        else { // 연결 될 때 까지 기다리기 (연결이 완료/실패되면 WifiListActivity의 wifiConnectReceiver에서 처리해줌)
-            Log.i(TAG, "연결 기다리기, supplicant state:"+wifiManager.getConnectionInfo().getSupplicantState());
-            connectingWiFiPosition = pos;
-            myWiFiList.get(pos).setState(WiFi.WIFI_CONNECTING);
-            notifyDataSetChanged();
-        }
+                if (netId == -1) { // 연결 실패한 경우
+                    Log.e(TAG, "addNetwork() returns -1.");
+                    myWiFiList.get(pos).setState(WiFi.WIFI_ERROR); // 오류 발생
+                    notifyDataSetChanged();
+                    AlertDialog.Builder failDialog = new AlertDialog.Builder(baseContext);
+                    failDialog.setTitle("연결을 실패했습니다. 다시 시도해주세요");
+                    failDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ;
+                        }
+                    });
+                    failDialog.show();
+                }
+                else { // 연결 될 때 까지 기다리기 (연결이 완료/실패되면 WifiListActivity의 wifiConnectReceiver에서 처리해줌)
+                    Log.i(TAG, "연결 기다리기, supplicant state:"+wifiManager.getConnectionInfo().getSupplicantState());
+                    connectingWiFiPosition = pos;
+                    myWiFiList.get(pos).setState(WiFi.WIFI_CONNECTING);
+                    notifyDataSetChanged();
+                }
+            }
+        });
+        saveDialog.setNegativeButton("저장 안 함", new DialogInterface.OnClickListener() { // 저장된 WiFi 삭제
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int netId = -1;
+                for (WifiConfiguration tmp : wifiManager.getConfiguredNetworks()) {
+                    if (tmp.SSID.equals("\"" +networkSSID+"\"")) {
+                        // Log.i(TAG, "똑같은거 찾음:"+networkSSID);
+                        netId = tmp.networkId;
+                        break;
+                    }
+                }
+                if (netId != -1 && wifiManager.removeNetwork(netId)) { // 저장된 WiFi 목록에서 삭제
+                    myWiFiList.get(pos).setState(WiFi.WIFI_NONE);
+                    notifyDataSetChanged();
+                } else { // 삭제 실패
+                    AlertDialog.Builder failDialog = new AlertDialog.Builder(baseContext);
+                    failDialog.setTitle("WiFi를 저장 해제하는 동안 오류가 발생했습니다. 다시 시도해주세요");
+                    failDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ;
+                        }
+                    });
+                    failDialog.show();
+                }
+            }
+        });
+        saveDialog.setCancelable(true);
+
+        saveDialog.show();
     }
 }
